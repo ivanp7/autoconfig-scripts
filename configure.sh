3572ccd87ea623569e1d87dab8cd60b236556fc2
ivanp7 <ivanp7.git@gmail.com>
2018-08-28 17:34:50 +0300

    Add vim installation instructions and improve existing code

--- configure.sh
+++ configure.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo
+echo ------------------------------------------------
 echo Arch Linux environment auto-configuration script
 echo ------------------------------------------------
 echo
@@ -9,26 +11,43 @@ if [[ $EUID != 0 ]]; then
     exit 1
 fi
 
-##########################################################################
+GIT_URL_PREFIX=https://gitlab.com/ivanp7
+echo
+echo "#################################################"
+echo
 
-read -p 'Enter user name: ' USERNAME
+read -p '> Enter user name: ' USERNAME
 
-echo Creating user \'$USERNAME\'...
+echo ] Creating user \'$USERNAME\'...
 useradd -m $USERNAME
-echo Setting password...
-passwd $USERNAME
+echo ] Setting password...
+until passwd $USERNAME; do echo "Try again"; sleep 2; done
 
-##########################################################################
+echo
+echo "#################################################"
+echo
 
-echo Installing sudo...
+echo ] Installing sudo...
 pacman --noconfirm -S sudo
-echo Configuring sudoers...
+echo ] Appending the following lines to sudoers...
 
 echo | EDITOR='tee -a' visudo
 echo "# User configuration" | EDITOR='tee -a' visudo
 echo "Defaults insults" | EDITOR='tee -a' visudo
 echo "%$USERNAME ALL=(ALL) ALL" | EDITOR='tee -a' visudo
 echo "%$USERNAME ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/poweroff,/usr/bin/halt,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/systemctl hibernate,/usr/bin/systemctl hybrid-sleep" | EDITOR='tee -a' visudo
+
+echo
+echo "#################################################"
+echo
+
+cd /home/$USERNAME/
+
+echo ] Installing vim...
+pacman --noconfirm -S vim
+echo ] Installing vim-config
+sudo -H -u $USERNAME (git clone $GIT_URL_PREFIX/vim-config.git .vim; vim +PlugUpdate)
+ln -s /home/$USERNAME/.vim /root/
 
 # echo Please, configure sudoers manually. visudo will be launched now.
 # read -n1 -rsp $'Press any key to continue...\n'
