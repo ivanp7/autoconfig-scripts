f85f4f35dacf3e94a3eb0c21b936f89407d67aaf
ivanp7 <ivanp7.git@gmail.com>
2018-08-31 02:16:37 +0300

    Add gnome-screenshot installation instruction and reorder other packages

--- configure-archlinux.sh
+++ configure-archlinux.sh
@@ -143,15 +143,11 @@ install_official_packages xorg xorg-xinit xorg-drivers
 print_message "Installing xclip, gtkmm3, compton, dex, fltk..."
 install_official_packages xclip gtkmm3 compton dex fltk
 
-print_message "Installing i3-gaps, i3status, dmenu..."
-install_official_packages i3-gaps i3status dmenu
+print_message "Installing i3-gaps, i3status, dmenu, conky, dunst, gnome-screenshot..."
+install_official_packages i3-gaps i3status dmenu conky dunst gnome-screenshot
 
-####################################################################
-
-print_message "Installing x-dotfiles..."
-run_as_nonroot git clone $GIT_URL_PREFIX/x-dotfiles.git
-chmod +x x-dotfiles/install.sh
-run_as_nonroot x-dotfiles/install.sh
+print_message "Installing termite..."
+install_official_packages termite
 
 ####################################################################
 
@@ -163,17 +159,19 @@ install_official_packages lxappearance
 
 ####################################################################
 
+print_message "Installing x-dotfiles..."
+run_as_nonroot git clone $GIT_URL_PREFIX/x-dotfiles.git
+chmod +x x-dotfiles/install.sh
+run_as_nonroot x-dotfiles/install.sh
+
+####################################################################
+
 print_message "Installing ALSA..."
 install_official_packages alsa-utils
 
 run_as_nonroot amixer sset Master unmute
 run_as_nonroot amixer sset Speaker unmute
 run_as_nonroot amixer sset Headphone unmute
-
-####################################################################
-
-print_message "Installing termite, dunst, conky..."
-install_official_packages termite dunst conky
 
 ####################################################################
 
