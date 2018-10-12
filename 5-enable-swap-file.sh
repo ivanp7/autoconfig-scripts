c32a0b852a5eeb8edf7badfec91ced6f2ef16193
ivanp7 <ivanp7.git@gmail.com>
2018-10-13 02:26:44 +0300

    Add script for swap file creation and enabling

---
+++ 5-enable-swap-file.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source `dirname $0`/functions.sh
+
+####################################################################
+
+print_message "#### Enabling swap file ####"
+
+####################################################################
+
+initialize
+
+####################################################################
+
+sudo fallocate -l $(awk '/MemTotal/ {print $2}' /proc/meminfo)k /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '# swap file\
+/swapfile none swap defaults 0 0\
+' | sudo tee -a /etc/fstab
+
+####################################################################
+
+finish
+
