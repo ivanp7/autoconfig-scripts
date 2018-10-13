24a36de68a834c74b2d3aa449ab7cb1159472e54
ivanp7 <ivanp7.git@gmail.com>
2018-10-13 03:44:41 +0300

    Fix sed command in hibernation enabling script

--- 6-enable-hibernation.sh
+++ 6-enable-hibernation.sh
@@ -17,7 +17,7 @@ sudo mkinitcpio -p linux
 
 SWAP_DEVICE=$(df / | sed '2q;d' | awk '{print $1}')
 SWAP_OFFSET=$(sudo filefrag -v /swapfile | sed '4q;d' | awk '{print $4}' | cut -d'.' -f1)
-sudo sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT=\"resume=$SWAP_DEVICE resume_offset=$SWAP_OFFSET /" /etc/default/grub
+sudo sed -i "s@GRUB_CMDLINE_LINUX_DEFAULT=\"@GRUB_CMDLINE_LINUX_DEFAULT=\"resume=$SWAP_DEVICE resume_offset=$SWAP_OFFSET @" /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 ####################################################################
