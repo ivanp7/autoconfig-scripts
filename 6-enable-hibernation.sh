390f3897917ce78218e9df3349b073d4bb083a1a
ivanp7 <ivanp7.git@gmail.com>
2018-10-13 03:39:01 +0300

    Fix incorrect swap file device query command

--- 6-enable-hibernation.sh
+++ 6-enable-hibernation.sh
@@ -15,7 +15,7 @@ initialize
 sudo sed -i "s/fsck/resume fsck/" /etc/mkinitcpio.conf
 sudo mkinitcpio -p linux
 
-SWAP_DEVICE=$(df / | awk '{print $1}')
+SWAP_DEVICE=$(df / | sed '2q;d' | awk '{print $1}')
 SWAP_OFFSET=$(sudo filefrag -v /swapfile | sed '4q;d' | awk '{print $4}' | cut -d'.' -f1)
 sudo sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT=\"resume=$SWAP_DEVICE resume_offset=$SWAP_OFFSET /" /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
