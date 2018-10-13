0e730354f648ebdd7a489a0a213fce5bbb7fb565
ivanp7 <ivanp7.git@gmail.com>
2018-10-13 04:37:29 +0300

    Add script for WOL enabling

---
+++ 8-enable-wol.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+source `dirname $0`/functions.sh
+
+####################################################################
+
+print_message "#### Enabling Wake-on-Lan ####"
+
+####################################################################
+
+initialize
+
+####################################################################
+
+NETWORK_INTERFACE=$(grep 'Interface=' /etc/netctl/network | cut -d'=' -f2)
+echo "ExecUpPost=\'/usr/bin/ethtool -s $NETWORK_INTERFACE wol g\'
+" | sudo tee -a /etc/netctl/network
+sudo netctl reenable network
+
+echo "!/bin/bash
+
+case \$1/\$2 in
+    pre/*)
+        # echo "Going to \$2..."
+        ;;
+    post/*)
+        # echo "Waking up from \$2..."
+        /usr/bin/ethtool -s $NETWORK_INTERFACE wol g
+        ;;
+esac
+" | sudo tee /usr/lib/systemd/system-sleep/50-reset-wol.sh
+chmod +x /usr/lib/systemd/system-sleep/50-reset-wol.sh
+
+####################################################################
+
+finish
+
