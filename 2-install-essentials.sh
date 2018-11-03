18c1ff57663c7d17d1a259fd0dfa22ce783be0ce
ivanp7 <ivanp7.git@gmail.com>
2018-11-03 20:02:30 +0300

    Move yaft installation instructions to a separate script

--- 2-install-essentials.sh
+++ 2-install-essentials.sh
@@ -46,29 +46,6 @@ sudo sh dotfiles/install.sh
 
 ####################################################################
 
-YAFT_TABSTOP=4
-YAFT_TERMINUS_FONT_VARIATION=u12n
-
-print_message "Installing yaft..."
-cd /tmp
-git clone https://aur.archlinux.org/yaft.git
-cd yaft
-makepkg --noconfirm -o
-YAFT_VER=$(cat PKGBUILD | grep ^pkgver= | cut -d'=' -f2)
-cd src/yaft-$YAFT_VER/
-# change TABSTOP
-sed -i "/^[[:blank:]]*TABSTOP[[:blank:]]*=/ s/8/$YAFT_TABSTOP/" conf.h
-# do not build default font
-sed -i '/^[[:blank:]]*.\/mkfont_bdf/ s/.\/mkfont_bdf/# .\/mkfont_bdf/' makefile
-# build Terminus font instead
-make mkfont_bdf
-cp $SCRIPT_DIR/aux/glyph_builder.sh ./
-sh glyph_builder.sh terminus $YAFT_TERMINUS_FONT_VARIATION
-cd ../..
-makepkg --noconfirm -ei
-
-####################################################################
-
 print_message "Installing yay..."
 cd /tmp
 git clone https://aur.archlinux.org/yay.git
