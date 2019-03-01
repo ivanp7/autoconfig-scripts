a371dd175a452f8d3404f09863015d96e6ff7bbe
ivanp7 <ivanp7.git@gmail.com>
2019-03-01 20:53:06 +0300

    Remove executable permissions from virtual machine shared folders

--- 1-essentials/4-install-vm-tools.sh
+++ 1-essentials/4-install-vm-tools.sh
@@ -39,7 +39,7 @@ echo "# VMWare Workstation shared folders" | sudo tee -a /etc/fstab
 for dir in $(vmware-hgfsclient)
 do
     sudo mkdir $SHARED_DIRECTORIES_MOUNT_LOCATION/$dir
-    echo ".host:/$dir $SHARED_DIRECTORIES_MOUNT_LOCATION/$dir fuse.vmhgfs-fuse rw,allow_other,uid=root,gid=vmhgfs,umask=0003,auto_unmount,defaults 0 0" | sudo tee -a /etc/fstab
+    echo ".host:/$dir $SHARED_DIRECTORIES_MOUNT_LOCATION/$dir fuse.vmhgfs-fuse rw,allow_other,uid=root,gid=vmhgfs,umask=0113,auto_unmount,defaults 0 0" | sudo tee -a /etc/fstab
 done
 echo | sudo tee -a /etc/fstab
 sudo mount -a
