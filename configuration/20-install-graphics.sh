fd4ffd0877ece614e66216f9f027bd668a822bca
ivanp7 <ivanp7.git@gmail.com>
2019-03-29 20:37:50 +0300

    Drop conky after transfer to polybar

--- configuration/20-install-graphics.sh
+++ configuration/20-install-graphics.sh
@@ -17,7 +17,7 @@ install_official_packages xorg xorg-xinit xorg-drivers
 uninstall_packages vim
 install_official_packages gvim
 install_official_packages xclip xsel gtkmm3 compton dex fltk
-install_official_packages i3-gaps dmenu conky libnotify dunst gnome-screenshot
+install_official_packages i3-gaps dmenu libnotify dunst gnome-screenshot
 install_packages polybar
 install_official_packages termite
 
