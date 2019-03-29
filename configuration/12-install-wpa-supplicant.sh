79af95a1e2b848709a04b4647d8b36912f94d07c
ivanp7 <ivanp7.git@gmail.com>
2019-03-29 20:35:51 +0300

    Change wireless interface getter command in wpa_supplicant installer

--- configuration/12-install-wpa-supplicant.sh
+++ configuration/12-install-wpa-supplicant.sh
@@ -15,7 +15,7 @@ initialize
 
 install_official_packages wpa_supplicant
 sudo install -Dm 644 $(aux_dir)/wpa_supplicant.conf /etc/wpa_supplicant/
-sudo wpa_supplicant -B -i $(cat /proc/net/wireless | tail -n1 | cut -d':' -f1) -c /etc/wpa_supplicant/wpa_supplicant.conf
+sudo wpa_supplicant -B -i $(ip link | egrep "^[[:digit:]]:" | cut -d' ' -f2 | tr -d : | egrep "^wl" | head -n1) -c /etc/wpa_supplicant/wpa_supplicant.conf
 
 ####################################################################
 
