a44ad21681f0d52435b3fccfd2ad00a9baa463a5
ivanp7 <ivanp7.git@gmail.com>
2019-04-01 01:26:27 +0300

    Simplify the way of adding user's sudoers configuration

--- configuration/02-add-user-sudoers.sh
+++ configuration/02-add-user-sudoers.sh
@@ -9,18 +9,18 @@ check_root
 
 ####################################################################
 
-print_message "#### Creating a maintainer user ####"
+print_message "#### Adding user sudoers configuration ####"
 
 ####################################################################
 
-until { read -p 'User name: ' USERNAME; useradd -m "$USERNAME"; }
-do echo "Try again"; sleep 2; done
-until passwd $USERNAME; do echo "Try again"; sleep 2; done
+if [ -f /tmp/archlinux-autoconfig-username ]
+then USERNAME=$(cat /tmp/archlinux-autoconfig-username)
+else
+    until read -p 'User name: ' USERNAME
+    do echo "Try again"; sleep 2; done
+fi
 
-echo $USERNAME > /tmp/archlinux-autoconfig-username
-
-print_message "Adding user to group 'shared'..."
-gpasswd -a $USERNAME shared
+cp $(aux_dir)/user_sudoers /etc/sudoers.d/${USERNAME}_sudoers
 
 ####################################################################
 
