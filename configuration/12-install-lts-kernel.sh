6594991855d9a0b6e01157b2d36436673ec42b1e
ivanp7 <ivanp7.git@gmail.com>
2019-04-02 15:12:40 +0300

    Add script for installing LTS kernel

---
+++ configuration/12-install-lts-kernel.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(realpath `dirname $0`)
+. $(realpath $SCRIPT_DIR/..)/functions.sh
+
+####################################################################
+
+print_message "#### Installing long term support kernel ####"
+
+####################################################################
+
+check_user
+
+####################################################################
+
+install_official_packages linux-lts
+uninstall_packages linux
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+####################################################################
+
+finish
+
