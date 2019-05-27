529e14fcc39d9f82a54670828bace342abcdd9d0
ivanp7 <ivanp7.git@gmail.com>
2019-05-27 14:34:06 +0300

    Enable SSH X11 forwarding by default

--- configuration/34-configure-ssh.sh
+++ configuration/34-configure-ssh.sh
@@ -25,6 +25,8 @@ sudo sed -i "s/^#PermitRootLogin .*/PermitRootLogin no/" /etc/ssh/sshd_config
 sudo sed -i "s/^#PubkeyAuthentication .*/PubkeyAuthentication yes/" /etc/ssh/sshd_config
 sudo sed -i "s/^#PasswordAuthentication .*/PasswordAuthentication no/" /etc/ssh/sshd_config
 sudo sed -i "s/^#PermitEmptyPasswords .*/PermitEmptyPasswords no/" /etc/ssh/sshd_config
+sudo sed -i "s/^#X11Forwarding .*/X11Forwarding yes/" /etc/ssh/sshd_config
+sudo sed -i "s/^#X11UseLocalhost .*/X11UseLocalhost yes/" /etc/ssh/sshd_config
 sudo sed -i "/^Subsystem.*sftp.*/ s/Subsystem/#Subsystem/" /etc/ssh/sshd_config
 
 cat $(aux_dir)/sshd_config_tail | sudo tee -a /etc/ssh/sshd_config
