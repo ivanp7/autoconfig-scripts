4fd20c1f49b033627f5cb476f821ea0e298878f9
ivanp7 <ivanp7.git@gmail.com>
2019-06-11 17:02:01 +0300

    Change installation path for autosuspend.sh

--- configuration/33-enable-autosuspend.sh
+++ configuration/33-enable-autosuspend.sh
@@ -13,7 +13,7 @@ check_user
 
 ####################################################################
 
-sudo install -Dm 755 $(aux_dir)/autosuspend.sh /usr/local/bin/
+sudo install -Dm 755 $(aux_dir)/autosuspend.sh /usr/local/bin/autosuspend/
 sudo install -Dm 755 $(aux_dir)/autosuspend_op.sh /usr/local/bin/
 sudo install -Dm 644 $(aux_dir)/autosuspend.service /etc/systemd/system/
 
