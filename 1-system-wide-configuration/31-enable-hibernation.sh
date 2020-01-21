8bff303fa5d4b170df0013e9fae9de1e904e0019
ivanp7 <ivanp7.git@gmail.com>
2020-01-21 20:39:53 +0300

    Use UUID instead of device name for hibernation

--- 1-system-wide-configuration/31-enable-hibernation.sh
+++ 1-system-wide-configuration/31-enable-hibernation.sh
@@ -17,8 +17,9 @@ sed -i "/^HOOKS=/ s/fsck/resume fsck/" /etc/mkinitcpio.conf
 mkinitcpio -P
 
 SWAP_DEVICE="$(df / | sed '2q;d' | awk '{print $1}')"
+SWAP_DEVICE_UUID="$(blkid | grep -F "$SWAP_DEVICE" | sed -E 's/.*UUID="(.*)".*/\1/')"
 SWAP_OFFSET="$(filefrag -v /swapfile | sed '4q;d' | awk '{print $4}' | cut -d'.' -f1)"
-SWAP_PARAMETERS="resume=${SWAP_DEVICE} resume_offset=${SWAP_OFFSET}"
+SWAP_PARAMETERS="resume=UUID=${SWAP_DEVICE_UUID} resume_offset=${SWAP_OFFSET}"
 sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/ s@\"@\"${SWAP_PARAMETERS} @" /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
