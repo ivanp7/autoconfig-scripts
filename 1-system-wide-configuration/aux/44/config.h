d1517aed0c02b8c812c6d2c938826d3e6bae8e26
ivanp7 <ivanp7.git@gmail.com>
2020-02-03 09:35:27 +0300

    Do not hardcode font into st

--- 1-system-wide-configuration/aux/44/config.h
+++ 1-system-wide-configuration/aux/44/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "xos4 Terminus:pixelsize=10:antialias=true:autohint=true";
-static char *font2[] = { "xos4 Terminus:pixelsize=10:antialias=true:autohint=true" };
+static char *font = "FONT_NAME:pixelsize=FONT_SIZE:antialias=true:autohint=true";
+static char *font2[] = { "FONT_NAME:pixelsize=FONT_SIZE:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
