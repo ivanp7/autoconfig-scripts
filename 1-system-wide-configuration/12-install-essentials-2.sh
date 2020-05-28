d44ba2ec4f2fd0495a98578cc524dd0f0589c1de
ivanp7 <ivanp7.git@gmail.com>
2020-05-28 13:12:40 +0300

    Replace unavailable package

--- 1-system-wide-configuration/12-install-essentials-2.sh
+++ 1-system-wide-configuration/12-install-essentials-2.sh
@@ -29,7 +29,7 @@ cd $SHARED_DIRECTORY
 ####################################################################
 
 print_message "Installing essentials from AUR..."
-install_packages lf archivemount 
+install_packages lf archivemount-git
 install_packages roswell quicklisp
 install_packages stardict-full-eng-rus stardict-full-rus-eng
 install_packages stardict-slang-eng-rus stardict-computer-ru
