8b18e38ebc0c25c9bd621e705bfb42e404f80f9a
ivanp7 <ivanp7.git@gmail.com>
2020-06-01 03:21:25 +0300

    Add fixes

--- 1-system-wide-configuration/22-configure-wpa-supplicant.sh
+++ 1-system-wide-configuration/22-configure-wpa-supplicant.sh
@@ -13,10 +13,8 @@ check_root
 
 ####################################################################
 
-install_official_packages wpa_supplicant
 install -Dm 644 $(aux_dir)/wpa_supplicant.conf /etc/wpa_supplicant/
-ip link | grep -E '^[[:digit:]]*: *wl.*:' | sed -E 's/^(.*): *(.*): (.*)/\2/' | head -n 1 | 
-    xargs -I {} wpa_supplicant -B -i {} -c /etc/wpa_supplicant/wpa_supplicant.conf
+sed -i '/^Exec=/ s@$@ -f /var/log/wpa_supplicant.log@' /usr/share/dbus-1/system-services/fi.w1.wpa_supplicant1.service
 
 ####################################################################
 
--- 1-system-wide-configuration/52-enable-nvidia-drm.sh
+++ 1-system-wide-configuration/52-enable-nvidia-drm.sh
@@ -13,6 +13,17 @@ check_root
 
 ####################################################################
 
+if pacman -Qi linux-lts > /dev/null 2>&1 
+then
+    install_official_packages linux-lts-headers
+    HOOK=nvidia-lts 
+else 
+    install_official_packages linux-headers
+    HOOK=nvidia
+fi
+install -Dm 644 $(aux_dir)/${HOOK}.hook /etc/pacman.d/hooks/
+install_official_packages nvidia-dkms
+
 grep -q '^MODULES=[^#]*nvidia' /etc/mkinitcpio.conf || {
     MODULES="nvidia nvidia_modeset nvidia_uvm nvidia_drm"
     sed -i "/^MODULES=/ s/(/($MODULES /" /etc/mkinitcpio.conf
@@ -23,9 +34,6 @@ grep -q '^GRUB_CMDLINE_LINUX_DEFAULT=[^#]*nvidia' /etc/default/grub || {
     sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/ s@\"@\"nvidia-drm.modeset=1 @" /etc/default/grub
     grub-mkconfig -o /boot/grub/grub.cfg
 }
-
-pacman -Qi linux-lts > /dev/null 2>&1 && HOOK=nvidia-lts || HOOK=nvidia
-install -Dm 644 $(aux_dir)/${HOOK}.hook /etc/pacman.d/hooks/
 
 ####################################################################
 
