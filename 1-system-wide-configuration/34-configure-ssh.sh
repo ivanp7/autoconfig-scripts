1073ab9e053d9d7b1e61f47ce08b7b29c254c95e
ivanp7 <ivanp7.git@gmail.com>
2020-06-20 14:53:08 +0300

    Disable PAM during SSH configuration

--- 1-system-wide-configuration/34-configure-ssh.sh
+++ 1-system-wide-configuration/34-configure-ssh.sh
@@ -27,6 +27,7 @@ sed -i "s/^#PermitRootLogin .*/PermitRootLogin no/" /etc/ssh/sshd_config
 sed -i "s/^#PubkeyAuthentication .*/PubkeyAuthentication yes/" /etc/ssh/sshd_config
 sed -i "s/^#PasswordAuthentication .*/PasswordAuthentication no/" /etc/ssh/sshd_config
 sed -i "s/^#PermitEmptyPasswords .*/PermitEmptyPasswords no/" /etc/ssh/sshd_config
+sed -i "s/^UsePAM .*/UsePAM no/" /etc/ssh/sshd_config
 sed -i "s/^#X11Forwarding .*/X11Forwarding yes/" /etc/ssh/sshd_config
 sed -i "s/^#X11UseLocalhost .*/X11UseLocalhost yes/" /etc/ssh/sshd_config
 sed -i "/^Subsystem.*sftp.*/ s/Subsystem/#Subsystem/" /etc/ssh/sshd_config
