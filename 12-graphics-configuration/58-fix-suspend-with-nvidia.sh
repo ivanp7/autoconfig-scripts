a5308360baf1fdd2b8c7ed7c1898e2f21e10cc08
ivanp7 <ivanp7.git@gmail.com>
2020-10-20 18:45:27 +0300

    Fix 58th script

--- 12-graphics-configuration/58-fix-suspend-with-nvidia.sh
+++ 12-graphics-configuration/58-fix-suspend-with-nvidia.sh
@@ -14,6 +14,7 @@ check_root
 ####################################################################
 
 install -Dm 644 $(aux_dir)/nvidia_copy /usr/lib/elogind/system-sleep/
+install -Dm 755 $(aux_dir)/nvidia_copy /usr/lib/elogind/system-sleep/nvidia
 install -Dm 644 $(aux_dir)/nvidia-utils.hook /etc/pacman.d/hooks/
 
 ####################################################################
