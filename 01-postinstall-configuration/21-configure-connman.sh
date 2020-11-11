a4fb6390ade25e07d026e307067e992c920181b2
ivanp7 <ivanp7@mail.ru>
2020-11-11 15:56:33 +0300

    Improve connman configuration script

--- 01-postinstall-configuration/21-configure-connman.sh
+++ 01-postinstall-configuration/21-configure-connman.sh
@@ -13,8 +13,9 @@ check_root
 
 ####################################################################
 
-sed -i "s/^# AllowHostnameUpdates =.*/AllowHostnameUpdates = false/" /etc/connman/main.conf
 sed -i "s/^# PreferredTechnologies =.*/PreferredTechnologies = ethernet,wifi/" /etc/connman/main.conf
+sed -i "s/^# NetworkInterfaceBlacklist =.*/NetworkInterfaceBlacklist = vnet,vmnet,vboxnet,virbr,ifb,ve-,vb-/" /etc/connman/main.conf
+sed -i "s/^# AllowHostnameUpdates =.*/AllowHostnameUpdates = false/" /etc/connman/main.conf
 sed -i "s/^# SingleConnectedTechnology =.*/SingleConnectedTechnology = true/" /etc/connman/main.conf
 
 sv restart connmand
