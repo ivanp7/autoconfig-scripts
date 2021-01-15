32e6b05e6d484133770ffb651878ba5e210b62cf
ivanp7 <ivanp7@mail.ru>
2021-01-15 13:01:50 +0300

    Add zathura-ps to essentials

--- 20-graphics-configuration/01-install-x-essentials.sh
+++ 20-graphics-configuration/01-install-x-essentials.sh
@@ -57,7 +57,7 @@ install_official_packages lxappearance
 
 print_message "#### Installing media tools ####"
 
-install_official_packages feh zathura zathura-pdf-mupdf zathura-djvu
+install_official_packages feh zathura zathura-pdf-mupdf zathura-djvu zathura-ps
 install_official_packages ffmpeg ffmpegthumbnailer
 
 print_message "#### Install Roswell applications ####"
