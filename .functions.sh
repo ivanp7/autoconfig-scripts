46195a89e87239e3418b64da40f2096b1cf63fd7
ivanp7 <ivanp7@mail.ru>
2021-03-09 00:01:33 +0300

    Add minor style improvement

--- .functions.sh
+++ .functions.sh
@@ -99,9 +99,9 @@ install_init_script ()
 
 install_keymap ()
 {
-    local MAP_FILE=/usr/local/share/kbd/keymaps/$(basename $1)
-    { dumpkeys | head -1; cat $1; } > $MAP_FILE
-    chmod 644 $MAP_FILE
+    local MAP_FILE="/usr/local/share/kbd/keymaps/$(basename "$1")"
+    { dumpkeys | head -1; cat "$1"; } > "$MAP_FILE"
+    chmod 644 "$MAP_FILE"
 }
 
 clone_git_repo_and_cd ()
