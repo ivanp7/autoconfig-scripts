fb6176941baa9a4c1560004ee49fd9769f26627b
ivanp7 <ivanp7@mail.ru>
2021-10-03 04:02:29 +0300

    Change dmenu font size according to $DEFAULT_FONT variable

--- 1-common/2-packages/2-graphical-environment/install-dmenu.req.sh
+++ 1-common/2-packages/2-graphical-environment/install-dmenu.req.sh
@@ -11,5 +11,9 @@ SCRIPT_TITLE="Install dmenu"
 
 git_clone_and_cd dmenu-ivanp7 $GIT_URL_PREFIX/dmenu-ivanp7.git
 download_and_extract_source
+
+HARDCODED_FONT="xos4 Terminus:size=12"
+[ -n "$DEFAULT_FONT" ] && sed -i "s/$HARDCODED_FONT/$DEFAULT_FONT/" config.h
+
 build_and_install_package
 
