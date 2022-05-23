ee3c019fe003f87d8415ffeb391d747f2851bb78
ivanp7 <ivanp7@mail.ru>
2022-05-23 23:33:40 +0300

    fix(nvidia-video): remove nvidia-lts package from installation list as it is conflicting with nvidia-dkms

--- 2-specific/nvidia-video/install-nvidia-driver.opt.sh
+++ 2-specific/nvidia-video/install-nvidia-driver.opt.sh
@@ -4,13 +4,10 @@ SCRIPT_NAME="$(basename "$0")"
 cd -- "$(dirname "$0")"
 SCRIPT_DIR="$PWD"
 
-SCRIPT_TITLE="Install NVIDIA graphics driver"
+SCRIPT_TITLE="Install auxilliaries for NVIDIA graphics driver"
 . "./.init.sh"
 
 ####################################################################
 
-install_packages linux-lts-headers nvidia-lts \
-    nvidia-settings libvdpau-va-gl nvtop
-
-install_pacman_hook nvidia-lts
+install_packages nvidia-settings libvdpau-va-gl nvtop
 
