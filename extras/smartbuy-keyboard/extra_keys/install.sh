09cdf646bc0c1a919c490a99e6e9420e51720749
ivanp7 <ivanp7@mail.ru>
2022-11-17 15:38:57 +0300

    feat: add CapsLock-to-Ctrl map, update smartbuy keyboard configuration

--- extras/smartbuy-keyboard/extra_keys/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd "$(dirname "$0")"
-MAP_FILE=smartbuy-keyboard.map
-{ sudo dumpkeys | head -1; cat "$MAP_FILE"; } | sudo tee -a /usr/local/share/kbd/keymaps/$MAP_FILE
-sudo chmod 644 /usr/local/share/kbd/keymaps/$MAP_FILE
-
-cat xmodmap >> "$XDG_CONFIG_HOME/X11/xmodmap"
-
