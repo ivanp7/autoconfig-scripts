177ece6608f643dbffa6a468adbcef939bbc7248
ivanp7 <ivanp7@mail.ru>
2022-11-17 14:05:43 +0300

    feat: add autostart script for loading VT keymaps

--- 1-common/4-services/init-scripts/.aux/enable-keymap-loading-script.opt.sh/console-keymap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for keymap in $(find /usr/local/share/kbd/keymaps/ -type f)
-do
-    loadkeys "$keymap"
-done
-
---
+++ configuration/3-system-custom/autostart-vt-keymaps/files/vt-keymaps.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+find /usr/local/share/kbd/keymaps -type f -exec loadkeys "{}" \;
+
